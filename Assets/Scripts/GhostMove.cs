22bf187972fac7bf376edccfb0799f355e2513e5
Gabriel Calderón <garry019@gmail.com>
2023-07-19 18:26:31 -0500

    Carga inicial de archivos

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostMove : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    [SerializeField] private Transform movePositionTransform;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination  = movePositionTransform.position;
+    }
+
+}
