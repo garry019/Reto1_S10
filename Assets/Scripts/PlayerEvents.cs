b4215ef6447dbfed78182ceea2c83f3e891f14e6
Gabriel Calderón <garry019@gmail.com>
2023-07-25 17:47:56 -0500

    All Basic Systems working

--- Assets/Scripts/PlayerEvents.cs
+++ Assets/Scripts/PlayerEvents.cs
@@ -8,24 +8,39 @@ using UnityEngine.SocialPlatforms.Impl;
 public class PlayerEvents : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI scoreText;
+    [SerializeField] public GameObject labyrinthKey;
 
     private int score = 0;
     public string gameOverText = "Game Over";
 
+    private void Update()
+    {
+        if (score == 139)
+        {
+            labyrinthKey.SetActive(true);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        /*if (collision.gameObject.CompareTag("Ghost"))
+        if (collision.gameObject.CompareTag("Ghost"))
         {
             Destroy(this);
             scoreText.text = gameOverText;
             PauseGame();
-        }*/
+        }
 
         if (collision.gameObject.CompareTag("EnergyCell"))
         {
             score++;
             scoreText.text = score.ToString();
             Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("LabyrinthKey"))
+        {
+            scoreText.text = "Win!!";
+            PauseGame();
         }
     }
 
