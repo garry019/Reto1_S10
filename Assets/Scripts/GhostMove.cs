c15457f0c0eda8f7a3ff94193a16038f60a7b227
Gabriel Calderón <garry019@gmail.com>
2023-07-25 12:45:19 -0500

    Ghost AI Updated

--- Assets/Scripts/GhostMove.cs
+++ Assets/Scripts/GhostMove.cs
@@ -2,26 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class GhostMove : MonoBehaviour
 {
 
+    
+    [SerializeField] private Transform Player;
+    public Vector3[] targetPositions;
     private NavMeshAgent agent;
-    [SerializeField] private Transform movePositionTransform;
+
+    bool chase = true;
+    bool scatter = false;
+    int nextPos = 0;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
+    private void Start()
+    {
+        if (targetPositions.Length < 3)
+        {
+            Debug.Log("Debes asignar las posiciones.");
+        }
+        else
+        {
+            StartCoroutine(GhostChase());
+            StartCoroutine(GhostScatter());
+        }
+        
+    }
+
     private void Update()
     {
-        attack();
+        //Verificación de posiciones asignadas
+        if (chase == true && targetPositions.Length == 3)
+        {
+            agent.destination = Player.position;
+            nextPos = 0;
+        }
+
+        if (scatter == true)
+        {
+            //Cambio de posición
+            if (nextPos <= 2)
+            {
+                agent.destination = targetPositions[nextPos];
+
+                if (agent.transform.position.x == targetPositions[nextPos].x && agent.transform.position.z == targetPositions[nextPos].z)
+                {
+                    nextPos++;
+                    agent.destination = targetPositions[nextPos];
+                }
+            }
+            else {
+                nextPos = 0;
+            }
+        }
     }
 
-    public void attack()
+    public void Chase()
     {
-        agent.destination = movePositionTransform.position;
+        chase = true;
+        scatter = false;
     }
 
+    public void Scatter()
+    {
+        chase = false;
+        scatter = true;
+    }
+
+    private IEnumerator GhostChase()
+    {
+
+        while (true)
+        {
+            yield return new WaitUntil(() => chase == true);
+            yield return new WaitForSeconds(20);
+            Scatter();
+        }
+    }
+
+    private IEnumerator GhostScatter()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(() => scatter == true);
+            yield return new WaitForSeconds(7);
+            Chase();
+        }
+    }
 }
